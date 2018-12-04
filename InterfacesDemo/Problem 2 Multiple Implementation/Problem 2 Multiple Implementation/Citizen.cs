be0535f9a00744b246cb84cb07509d09e10896f0
dimodd1 <dimodd1@yahoo.com>
2018-12-04 14:16:13 +0200

    Add problem 2 and its solution

---
+++ InterfacesDemo/Problem 2 Multiple Implementation/Problem 2 Multiple Implementation/Citizen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IIdentifiable, IBirthable
+    {
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            this.Birthdate = birthdate;
+            this.Id = id;
+        }
+
+        public string Birthdate { get; private set; }
+
+        public string Id { get; private set; }
+    }
+}
