1cf14335314393a2704ce7a90558e3a461f362e7
dimodd1 <dimodd1@yahoo.com>
2018-12-04 14:06:33 +0200

    Add first problem and its solution

---
+++ InterfacesDemo/Problem 2 Multiple Implementation/Problem 2 Multiple Implementation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problem_2_Multiple_Implementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
