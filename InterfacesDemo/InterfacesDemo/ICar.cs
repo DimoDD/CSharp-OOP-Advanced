96546069e61441d872f04fe7aab784dcc30055c3
Dimo Delchev <dimodd1@yahoo.com>
2018-12-04 12:31:26 +0200

    Add files via upload

---
+++ InterfacesDemo/InterfacesDemo/ICar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public interface ICar
+    {
+        string Model { get; }
+        string Color { get; }
+
+        void Start();
+        void Stop();
+        
+    }
+}
---
+++ InterfacesDemo/InterfacesDemo/Seat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+
+        public string Model { get; private set; }
+        public string Color { get; private set; }
+
+        public void Start()
+        {
+            Console.WriteLine("BRUM"); ;
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Brrr");
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
