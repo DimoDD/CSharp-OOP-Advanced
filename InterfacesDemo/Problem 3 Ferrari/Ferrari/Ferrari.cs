e858ea836a605d5d5677669c0c636218de76617e
dimodd1 <dimodd1@yahoo.com>
2018-12-04 14:30:20 +0200

    Add problem 3 and its solution

---
+++ InterfacesDemo/Problem 3 Ferrari/Ferrari/Ferrari.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ferrari
+{
+    public class Ferrari : IDriveable
+    {
+        public Ferrari(string driver)
+        {
+            this.Model = "488-Spider";
+            this.Driver = driver;
+        }
+
+        public string Model { get; private set; }
+        public string Driver { get; private set; }
+
+        public string PushGas()
+        {
+            return "Zadu6avam sA!";
+        }
+
+        public string UseBrakes()
+        {
+            return "Brakes!";
+        }
+
+        public override string ToString()
+        {
+            return $"{Model}/{UseBrakes()}/{PushGas()}/{Driver}";
+        }
+    }
+}
---
+++ InterfacesDemo/Problem 3 Ferrari/Ferrari/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ferrari
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string driver = Console.ReadLine();
+            var ferrari = new Ferrari(driver);
+            Console.WriteLine(ferrari);
+        }
+    }
+}
